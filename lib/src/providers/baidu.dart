c06d3728c9b8dd645bf7a5375a0f46c9b9fc5656
eryan <eryanwcp@gmail.com>
2023-03-23 15:00:38 +0800

    custom

--- lib/src/providers/baidu.dart
+++ lib/src/providers/baidu.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter_baidu_mapapi_base/flutter_baidu_mapapi_base.dart'
+    show BMFLog, BMFMapSDK, BMF_COORD_TYPE;
 import 'package:flutter_bmflocation/flutter_bmflocation.dart';
 import 'package:geolocator_cn/src/types.dart';
 
@@ -51,21 +53,48 @@ class LocationServiceProviderBaidu implements LocationServiceProvider {
     if (inited) {
       return;
     }
+    /// 设置是否隐私政策
+    /// 隐私政策官网链接：https://lbsyun.baidu.com/index.php?title=openprivacy
+    /// 通知用户之后根据用户选择进行赋值
     _locationPlugin.setAgreePrivacy(true);
+    BMFMapSDK.setAgreePrivacy(true);
+
     /// 设置ios端ak, android端ak可以直接在清单文件中配置
     if (Platform.isIOS) {
       /// 设置ios端ak, android端ak可以直接在清单文件中配置
       _locationPlugin.authAK(iosKey);
+      BMFMapSDK.setApiKeyAndCoordType(iosKey, BMF_COORD_TYPE.BD09LL);
+    }else if (Platform.isAndroid) {
+      // Android 目前不支持接口设置Apikey,
+      // 请在主工程的Manifest文件里设置，详细配置方法请参考官网(https://lbsyun.baidu.com/)demo
+      BMFMapSDK.setCoordType(BMF_COORD_TYPE.BD09LL);
     }
 
-
-
-    _locationPlugin.seriesLocationCallback(callback: (BaiduLocation result) {
-      if (result.latitude != null && result.longitude != null) {
-        _lastResult = result;
-        _stopLocation();
-      }
+    _locationPlugin.getApiKeyCallback(callback: (String result) {
+      String str = result;
+      print('百度地图鉴权结果：' + str);
     });
+
+
+    ///单次定位时如果是安卓可以在内部进行判断调用连续定位
+    if (Platform.isIOS) {
+      ///接受定位回调
+      _locationPlugin.singleLocationCallback(callback: (BaiduLocation result) {
+        if (result.latitude != null && result.longitude != null) {
+          _lastResult = result;
+          _stopLocation();
+        }
+      });
+    } else if (Platform.isAndroid) {
+      ///接受定位回调
+      _locationPlugin.seriesLocationCallback(callback: (BaiduLocation result) {
+        if (result.latitude != null && result.longitude != null) {
+          _lastResult = result;
+          _stopLocation();
+        }
+      });
+    }
+
 
     inited = true;
   }
@@ -74,7 +103,12 @@ class LocationServiceProviderBaidu implements LocationServiceProvider {
   void _startLocation() {
     try {
       _setLocOption();
-      _locationPlugin.startLocation();
+      // _locationPlugin.startLocation();
+      if (Platform.isIOS) {
+       _locationPlugin.singleLocation({'isReGeocode': true, 'isNetworkState': true});
+      } else if (Platform.isAndroid) {
+        _locationPlugin.startLocation();
+      }
     } catch (e) {
       print(e);
     }
@@ -92,20 +126,22 @@ class LocationServiceProviderBaidu implements LocationServiceProvider {
   /// 设置android端和ios端定位参数
   void _setLocOption() {
     /// android 端设置定位参数
-    BaiduLocationAndroidOption androidOption = BaiduLocationAndroidOption(coordType: BMFLocationCoordType.bd09ll);
-    androidOption.setIsNeedAltitude(false); // 设置是否需要返回海拔高度信息
-    androidOption.setIsNeedAddress(false); // 设置是否需要返回地址信息
-    androidOption.setIsNeedLocationPoiList(false); // 设置是否需要返回周边poi信息
-    androidOption.setIsNeedNewVersionRgc(false); // 设置是否需要返回最新版本rgc信息
-    androidOption.setIsNeedLocationDescribe(false); // 设置是否需要返回位置描述
-    androidOption.setOpenGps(true); // 设置是否需要使用gps
-    androidOption.setLocationMode(BMFLocationMode.hightAccuracy); // 设置定位模式
+    BaiduLocationAndroidOption androidOption = BaiduLocationAndroidOption(coordType: BMFLocationCoordType.bd09ll,
+    locationMode: BMFLocationMode.hightAccuracy,
+    isNeedAddress: false,
+    isNeedAltitude: false,
+    isNeedLocationPoiList: false,
+    isNeedNewVersionRgc: false,
+    openGps: true,
+    locationPurpose: BMFLocationPurpose.signIn);
     androidOption.setScanspan(1000); // 设置发起定位请求时间间隔
 
     Map androidMap = androidOption.getMap();
 
     /// ios 端设置定位参数
-    BaiduLocationIOSOption iosOption = BaiduLocationIOSOption(coordType: BMFLocationCoordType.bd09ll);
+    BaiduLocationIOSOption iosOption = BaiduLocationIOSOption(coordType: BMFLocationCoordType.bd09ll,
+        BMKLocationCoordinateType: 'BMKLocationCoordinateTypeBMK09LL',
+        desiredAccuracy: BMFDesiredAccuracy.best);
     iosOption.setIsNeedNewVersionRgc(true); // 设置是否需要返回最新版本rgc信息
     iosOption.setActivityType(BMFActivityType.fitness); // 设置应用位置类型
     iosOption.setLocationTimeout(10); // 设置位置获取超时时间
